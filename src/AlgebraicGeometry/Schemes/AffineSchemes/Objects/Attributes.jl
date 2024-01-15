0f557e232c6282d285e3a0f9bef7da90c52067da
Kamillo Ferry <kafe@kafe.dev>
2024-01-15 22:02:28 +0100

    Degree for finite affine schemes

--- src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Attributes.jl
+++ src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Attributes.jl
@@ -481,11 +481,9 @@ julia> codim(Y)
   return dim(ideal(ambient_coordinate_ring(X), [zero(ambient_coordinate_ring(X))])) - dim(X)
 end
 
-function degree(X::AffineScheme{BRT, RT}; check::Bool=true) where {BRT<:Field, RT}
-  @check dim(X) == 0 "the affine scheme X needs to be zero-dimensional"
-  get_attribute!(X, :degree) do
-    return vector_space_dimension(OO(X))
-  end
+@attr function degree(X::Spec{BRT, RT}) where {BRT<:Field, RT}
+  @req dim(X) == 0 "The affine scheme X needs to be zero-dimensional"
+  return vector_space_dimension(OO(X))
 end
 
 @doc raw"""
